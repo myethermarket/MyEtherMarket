997f61c1c8adf1f96021fdf01025bc95caea72b2
andrew <andrew@MyEtherMarket.com>
2017-11-04 16:15:48 -0700

    Added tokens and withdraw/deposit of eth
    (title is enough)

--- contract/myethermarket.sol
+++ contract/myethermarket.sol
@@ -22,7 +22,94 @@ contract SafeMath {
 
 }
 
-contract MyEtherMarket {
+contract Token {
+  /// @return total amount of tokens
+  function totalSupply() constant returns (uint256 supply) {}
+
+  /// @param _owner The address from which the balance will be retrieved
+  /// @return The balance
+  function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+  /// @notice send `_value` token to `_to` from `msg.sender`
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transfer(address _to, uint256 _value) returns (bool success) {}
+
+  /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+  /// @param _from The address of the sender
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+  /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @param _value The amount of wei to be approved for transfer
+  /// @return Whether the approval was successful or not
+  function approve(address _spender, uint256 _value) returns (bool success) {}
+
+  /// @param _owner The address of the account owning tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @return Amount of remaining tokens allowed to spent
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  uint public decimals;
+  string public name;
+}
+
+contract StandardToken is Token {
+
+  function transfer(address _to, uint256 _value) returns (bool success) {
+    //Default assumes totalSupply can't be over max (2^256 - 1).
+    //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+    //Replace the if with this one instead.
+    if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+    //if (balances[msg.sender] >= _value && _value > 0) {
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      Transfer(msg.sender, _to, _value);
+      return true;
+    } else { return false; }
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    //same as above. Replace this line with the following if you want to protect against wrapping uints.
+    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+    //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+      balances[_to] += _value;
+      balances[_from] -= _value;
+      allowed[_from][msg.sender] -= _value;
+      Transfer(_from, _to, _value);
+      return true;
+    } else { return false; }
+  }
+
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+  function approve(address _spender, uint256 _value) returns (bool success) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  mapping(address => uint256) balances;
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+  uint256 public totalSupply;
+}
+
+contract MyEtherMarket is SafeMath {
 
     struct Bid {
         // bids automatically have tokenGet as address, know tokenGive is ether
@@ -54,14 +141,29 @@ contract MyEtherMarket {
     // mappings of orders. key is order hash
     mapping (bytes32 => Bid) bids;
     mapping (bytes32 => Ask) asks;
-    //mapping (bytes32 => bool) orders; // prevents any possible duplicate orders
     // order books connect to book root only. key is [tokenAddr].
     mapping (address => bytes32) bookBidRoot;
     mapping (address => bytes32) bookAskRoot;
-
+    // MyEtherMarket wallet balances. keys are [tokenAddr][userAddr]; (token=0 means Ether)
+    mapping (address => mapping (address => uint)) public walletBalance;
     // constructor, can only be ran on initial contract upload
     function MyEtherMarket() {
         admin = msg.sender;
+    }
+    
+    
+    // ---------------------------   DEPOSIT/WITHDRAWAL    -------------------------
+    // ETHER, uses msg.value
+    function deposit() payable {
+        walletBalance[0][msg.sender] = safeAdd(walletBalance[0][msg.sender], msg.value);
+    }
+    
+    function withdraw(uint amount) {
+        assert(walletBalance[0][msg.sender] >= amount);
+        walletBalance[0][msg.sender] = safeSub(walletBalance[0][msg.sender], amount);
+        if (!msg.sender.call.value(amount)()) {
+            require(false); // throw
+        }
     }
     
     // ---------------------------------   BIDS    ---------------------------------
@@ -118,6 +220,14 @@ contract MyEtherMarket {
         }
         if (bids[hash].bookNext != 0) {
             bids[bids[hash].bookNext].bookPrev = bids[hash].bookPrev;
+        }
+    }
+    
+    // this is called internally to fill a bid in the book
+    function fillBid(address token, bytes32 hash, bool totalFill) private {
+        if (!totalFill) {
+            // give tokens
+            
         }
     }
     
